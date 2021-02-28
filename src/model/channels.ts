332db1f8ba261a458d0340a3899091dc1195e18a
Pablo Klaschka <contact@pabloklaschka.de>
2021-02-28 17:44:03 +0100

    feat: Add mock server plugin
    Co-authored-by: Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>

---
+++ src/model/channels.ts
@@ -0,0 +1,14 @@
+import { JsonSerializable } from '@wuespace/telestion-client-types';
+
+export const MONGODB_NEW =
+	'org.telestion.core.database.MongoDatabaseService/out#save';
+
+export const NineDOF =
+	MONGODB_NEW + '/de.jvpichowski.rocketsound.messages.base.NineDofData';
+
+export interface DataMessage<T extends JsonSerializable> {
+	[key: string]: JsonSerializable;
+	dataType: string;
+	result: T[];
+	className: string;
+}
