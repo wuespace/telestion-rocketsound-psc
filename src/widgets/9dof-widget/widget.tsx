0add43ad6bd4ec78b86bffc2c2644d725e9a0606
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-03-01 16:42:53 +0000

    feat(widget): Enhance 9dof table widget (#56)
    - define types for table column and item
    - new component Table
    - clean up imports
    - use title from user configuration
    
    Co-authored-by: Pablo Klaschka <pklaschka@users.noreply.github.com>

---
+++ src/widgets/9dof-widget/widget.tsx
@@ -0,0 +1,48 @@
+import { Heading, View } from '@adobe/react-spectrum';
+import { useChannelLatest } from '@wuespace/telestion-client-core';
+import { LoadingIndicator } from '@wuespace/telestion-client-common';
+import { BaseRendererProps } from '@wuespace/telestion-client-types';
+
+import { NineDofMessage } from '../../model/messages/nine-dof';
+import { NineDOF } from '../../model/channels';
+
+import { Table } from './table';
+import { Column, Item } from './model';
+
+const columns: Column[] = [
+	{ name: 'Sensor', key: 'sensor' },
+	{ name: <code>x</code>, key: 'x' },
+	{ name: <code>y</code>, key: 'y' },
+	{ name: <code>z</code>, key: 'z' }
+];
+
+export function Widget({ title }: BaseRendererProps) {
+	const latestData = useChannelLatest<NineDofMessage>(NineDOF)?.result[0];
+
+	const items: Item[] = [
+		{
+			sensor: 'Accelerometer',
+			key: `acc-${latestData ? Object.values(latestData?.acc) : ''}`,
+			...latestData?.acc
+		},
+		{
+			sensor: 'Gyroscope',
+			key: `gyro-${latestData ? Object.values(latestData?.gyro) : ''}`,
+			...latestData?.gyro
+		},
+		{
+			sensor: 'Magnetometer',
+			key: `mag-${latestData ? Object.values(latestData?.mag) : ''}`,
+			...latestData?.mag
+		}
+	];
+
+	return (
+		<View marginX={'size-200'}>
+			<Heading level={3}>{title}</Heading>
+			<LoadingIndicator dependencies={[latestData]}>
+				{() => <Table columns={columns} items={items} />}
+			</LoadingIndicator>
+		</View>
+	);
+}
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -7,6 +7,7 @@ using the tc-cli generate widget command.
 
 import { Widget } from '@wuespace/telestion-client-types';
 import { widget as sampleWidget } from './sample-widget';
+import { widget as nineDofWidget } from './9dof-widget';
 import { widget as stateWidget } from './state-widget';
 import { widget as ninedof } from './9dof';
 import { widget as graphWidget } from './graph-widget';
@@ -16,6 +17,6 @@ export const projectWidgets: Widget[] = [
 	// ARRAY_FIRST_ELEMENT_INSERT_MARK
 	stateWidget,
 	graphWidget as Widget,
-	ninedof,
+	nineDofWidget,
 	sampleWidget
 ];
