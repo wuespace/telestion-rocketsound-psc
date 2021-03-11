fda5138d8b10b9981fbb6ce7234cb5aeb3ac3990
Felix-Auinger <69576590+Felix-Auinger@users.noreply.github.com>
2021-03-11 22:50:06 +0100

    feat(widget): Enhance Map widget
    Add action button to return to last known position or if undefined to map center.
    
    Co-authored-by: Felix-Auinger <felix.auinger@t-online.de>
    Co-authored-by: Pablo Klaschka <pklaschka@users.noreply.github.com>
    Co-authored-by: Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>

--- src/widgets/map-widget/widget.tsx
+++ src/widgets/map-widget/widget.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { useChannelLatest } from '@wuespace/telestion-client-core';
-import { Flex, Heading, Switch } from '@adobe/react-spectrum';
+import { Flex, Heading, Switch, ActionButton } from '@adobe/react-spectrum';
 import { LatLng, LatLngBounds, Map } from 'leaflet';
 import {
 	CircleMarker,
 	MapContainer,
 	TileLayer,
-	MapConsumer
+	MapConsumer,
+	ScaleControl
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet/dist/leaflet.js';
@@ -26,6 +27,8 @@ export function Widget() {
 
 	let [isBounded, setBounded] = useState(true);
 
+	let [map, setMap] = useState<Map>();
+
 	const bounds = isBounded ? mapBounds : noBounds;
 
 	return (
@@ -37,6 +40,16 @@ export function Widget() {
 				marginStart="size-200"
 			>
 				<Heading>Map</Heading>
+				<ActionButton
+					onPress={() =>
+						map?.panTo([
+							latestMessage?.result[0].latitude ?? mapCenter.lat,
+							latestMessage?.result[0].longitude ?? mapCenter.lng
+						])
+					}
+				>
+					Recenter
+				</ActionButton>
 				<Switch isSelected={!isBounded} onChange={state => setBounded(!state)}>
 					Expert Mode (disable boundaries)
 				</Switch>
@@ -47,6 +60,7 @@ export function Widget() {
 				zoom={10}
 				minZoom={10}
 				maxZoom={15}
+				whenCreated={setMap}
 			>
 				<MapConsumer>
 					{(map: Map) => {
@@ -54,14 +68,18 @@ export function Widget() {
 						return null;
 					}}
 				</MapConsumer>
-				<TileLayer
-					attribution="&copy;"
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
+
+				<ScaleControl />
+
 				<TileLayer
 					attribution="&copy;"
 					url="http://localhost:4400/tiles/{z}/{x}/{y}.png"
 				/>
+				<TileLayer
+					attribution="&copy;"
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+
 				{latestMessage && (
 					<CircleMarker
 						center={[
