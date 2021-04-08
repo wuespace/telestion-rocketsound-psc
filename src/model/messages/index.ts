c7d29ebd52f763830f25daa88bbab198e5edffe0
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-04-08 03:39:23 +0200

    fix: Mock server plugin

--- src/model/messages/index.ts
+++ src/model/messages/index.ts
@@ -1,4 +1,7 @@
 export * from './amplitude';
+export * from './baro-data';
 export * from './flight-state';
 export * from './gps-data';
 export * from './nine-dof';
+export * from './spectrum';
+export * from './velocity';
